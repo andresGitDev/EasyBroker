2d7ddae10024bdf7affd1642ed207c0e082d5bdc
Nicolas <nicocrisgonzalez@gmail.com>
2022-06-23 21:29:13 -0300

    First commit

---
+++ src/main/java/com/easybroker/EasybrokerApplication.java
@@ -0,0 +1,13 @@
+package com.easybroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasybrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasybrokerApplication.class, args);
+	}
+
+}
